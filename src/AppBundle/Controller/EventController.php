998d725d12e4caa2ae1c4534642f29239501a837
Gaincoder <gaincoder+git@gmail.com>
2018-08-22 22:48:16 +0200

    new features for events

--- src/AppBundle/Controller/EventController.php
+++ src/AppBundle/Controller/EventController.php
@@ -177,7 +177,7 @@ class EventController extends Controller
 
         if($event->disableImpulse != true){
             $impulseBtn = ["text"=>'Spontan','callback_data'=>$impulse];
-            $btns[0][] = $impulseBtn;
+            $btns[] = $impulseBtn;
         }
         return new InlineKeyboardMarkup($btns);
 
