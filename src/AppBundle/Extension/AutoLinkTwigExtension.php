7803fe9418a3766a4aef0ac3512f0e491dbfdceb
Gaincoder <gaincoder+git@gmail.com>
2018-08-29 19:24:44 +0200

    fix for links

--- src/AppBundle/Extension/AutoLinkTwigExtension.php
+++ src/AppBundle/Extension/AutoLinkTwigExtension.php
@@ -25,27 +25,29 @@ class AutoLinkTwigExtension extends \Twig_Extension
 
     static public function auto_link_text($string)
     {
-        $string = htmlentities($string);
         $text = preg_replace(
             '#((https?|ftp)://(\S*?\.\S*?))([\s)\[\]{},;"\':<]|\.\s|$)#i',
-            "<a href=\"$1\" target=\"_blank\">__LINK__</a>$4",
+            "<a href=\"$1\" target=\"_blank\">$1</a>$4",
             $string
         );
 
-        if(strlen($text)) {
-            $dom = new \DOMDocument('1.0','UTF-8');
-            $text = $html_src = '<html><head><meta content="text/html; charset=utf-8" http-equiv="Content-Type"></head><body>'.$text;
-            $dom->loadHTML($text);
-            /** @var \DOMNode $node */
-            foreach ($dom->getElementsByTagName('a') as $node) {
-                $inner = $node->attributes->getNamedItem('href')->nodeValue;
-                if (strlen($inner) > 35) {
-                    $inner = substr($inner, 0, 30) . '...' . substr($inner, strlen($inner) - 5, 5);
+        try {
+            if (strlen($text)) {
+                $dom = new \DOMDocument('1.0', 'UTF-8');
+                $dom->strictErrorChecking = false;
+                $text = $html_src = '<html><head><meta content="text/html; charset=utf-8" http-equiv="Content-Type"></head><body>' . $text;
+                $dom->loadHTML($text);
+                foreach ($dom->getElementsByTagName('a') as $node) {
+                    $inner = $node->nodeValue;
+                    if (strlen($inner) > 35) {
+                        $inner = substr($inner, 0, 30) . '...' . substr($inner, strlen($inner) - 5, 5);
+                    }
+                    $node->nodeValue = $inner;
                 }
-                $node->nodeValue = $inner;
+                $text = $dom->saveHTML();
             }
-            $text = $dom->saveHTML();
-        }
+        }catch(\DOMException $exception)
+        {}
         return $text;
     }
 }
